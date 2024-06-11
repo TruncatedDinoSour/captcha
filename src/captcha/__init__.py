9b31c487d975b2ff8fc8b664d89eb61a8848c028
Ari Archer <ari@ari.lt>
2024-06-11 19:17:21 +0300

    1.1.0
    Signed-off-by: Ari Archer <ari@ari.lt>

--- src/captcha/__init__.py
+++ src/captcha/__init__.py
@@ -10,7 +10,7 @@
     :license: BSD, see LICENSE for more details.
 """
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 __author__ = 'Hsiaoming Yang <me@lepture.com>'
 __co_author__ = 'Ari Archer <ari@ari.lt>'
 __homepage__ = 'https://ari.lt/gh/captcha'
